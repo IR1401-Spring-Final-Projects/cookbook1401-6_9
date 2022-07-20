b4aba726d21b16bf971049a2584cb4ab56a42df7
ckodser <arshia.soltani2@gmail.com>
2022-07-21 00:18:09 +0430

    download foods

--- AIRProject/__init__.py
+++ AIRProject/__init__.py
@@ -1,11 +1,11 @@
 from searches import fasttext_search, preprocess, boolean, tfidf, transformer
 
-# preprocess.download_foods()
+preprocess.download_foods()
 
 preprocess.normalize_foods()
 preprocess.preprocess_all_searches()
 boolean.preprocess()
-# tfidf.preprocess()
+tfidf.preprocess()
 # transformer.preprocess()
-# fasttext_search.preprocess()
+fasttext_search.preprocess()
 # elastic_search.preprocess()
