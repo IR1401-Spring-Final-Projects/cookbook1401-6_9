827fa813b6c20106be966951814eced3cebebaa3
hkalbasi <hamidrezakalbasi@protonmail.com>
2022-07-27 22:28:30 +0430

    change view of collection

--- NextWords/views.py
+++ NextWords/views.py
@@ -3,7 +3,10 @@ from searches import next_words_prediction, spell_correction
 
 
 def index(request, search, text):
-    options = spell_correction.correct_spelling_candidates(text)
-    options.extend(next_words_prediction.next_words_options(text))
+    correction = spell_correction.correct_spelling_candidates(text)
+    options = next_words_prediction.next_words_options(text)
     print(options)
-    return render(request, "inner_word_prediction.html", {"text": text, "options": options, "search": search})
+    return render(request, "inner_word_prediction.html", {
+        "text": text, "options": options, "search": search,
+        "correction": correction,
+    })
