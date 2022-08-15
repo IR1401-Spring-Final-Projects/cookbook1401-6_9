0596cbb26dcb9b1417977a97211f25a54024ad87
ckodser <arshia.soltani2@gmail.com>
2022-08-15 13:50:20 +0430

    add favicon.ico

--- searchEng/urls.py
+++ searchEng/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('favicon.ico/', views.favicon, name='ii')
 ]
--- searchEng/views.py
+++ searchEng/views.py
@@ -6,3 +6,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "searchEng.html",{"text": "", "options": [""]*5, "search": "boolean"})
+
+def favicon(request):
+    print("OHHH")
+    image_data = open("static/favicon.ico", "rb").read()
+    return HttpResponse(image_data, content_type="image/x-icon")
